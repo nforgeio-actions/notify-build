3088d32be85a87cd10a5d963e6198acaa517c2d0
Jeff Lill <jeff@lilltek.com>
2021-04-19 19:34:25 -0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -19,6 +19,7 @@
 #   elapsedTime     - Elapsed operation run time
 #   status          - Operation status, one of: 'ok', 'warning', or 'failed'
 
+[CmdletBinding()]
 param
 (
     [Parameter(Position=0, Mandatory=1)]
