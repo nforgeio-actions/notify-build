10586b75b2cee8ff4705c777f803ebb070903b30
Jeff Lill <jeff@lilltek.com>
2021-04-27 13:43:59 -0700

    debugging...

--- action.ps1
+++ action.ps1
@@ -42,149 +42,167 @@ $ncPowershell = [System.IO.Path]::Combine($ncRoot, "Powershell")
 Push-Location $ncPowershell
 . ./includes.ps1
 Pop-Location
-      
-# Fetch the inputs.
 
-$channel        = Get-ActionInput "channel"          $true
-$operation      = Get-ActionInput "operation"        $true
-$buildBranch    = Get-ActionInput "build-branch"     $false
-$buildCommit    = Get-ActionInput "build-commit"     $false
-$buildCommitUri = Get-ActionInput "build-commit-uri" $false
-$startTime      = Get-ActionInput "start-time"       $false
-$finishTime     = Get-ActionInput "finish-time"      $false
-$buildOutcome   = Get-ActionInput "build-outcome"    $true
-$workflowRef    = Get-ActionInput "workflow-ref"     $true
-$sendOn         = Get-ActionInput "send-on"          $false
+# Implement the operation.
 
-# Exit if the notification shouldn't be transmitted based on the build outcome.
-# We're going to do a simple string match here rather than parsing [send-on].
+try
+{      
+    # Fetch the inputs.
 
-$sendAlways = ($sendOn -eq $null) -or ($sendOn.Contains("always"))
+    $channel        = Get-ActionInput "channel"          $true
+    $operation      = Get-ActionInput "operation"        $true
+    $buildBranch    = Get-ActionInput "build-branch"     $false
+    $buildCommit    = Get-ActionInput "build-commit"     $false
+    $buildCommitUri = Get-ActionInput "build-commit-uri" $false
+    $startTime      = Get-ActionInput "start-time"       $false
+    $finishTime     = Get-ActionInput "finish-time"      $false
+    $buildOutcome   = Get-ActionInput "build-outcome"    $true
+    $buildSuccess   = $(Get-ActionInput "build-success" $True) -eq "true"
+    $workflowRef    = Get-ActionInput "workflow-ref"     $true
+    $sendOn         = Get-ActionInput "send-on"          $false
 
-if (!$sendAlways -and !$sendOn.Contains($buildOutcome))
-{
-    return
-}
+    # Exit if the notification shouldn't be transmitted based on the build outcome.
+    # We're going to do a simple string match here rather than parsing [send-on].
 
-# Handle missing [build-branch] and [build-commit-uri] inputs.
+    $sendAlways = ($sendOn -eq $null) -or ($sendOn.Contains("always"))
 
-if ([System.String]::IsNullOrEmpty($buildBranch))
-{
-    $buildBranch = "-na-"
-}
-else
-{
-    $buildBranch = "**$buildBranch**"
-}
-
-if ([System.String]::IsNullOrEmpty($buildCommit) -or [System.String]::IsNullOrEmpty($buildCommitUri))
-{
-    $buildCommitUri = "-na-"
-}
-else
-{
-    $buildCommitUri = "[$buildCommit]($buildCommitUri)"
-}
-
-# Parse the optional start/finish times and compute the elapsed time.  Note that
-# we're going to display "-na" when either of these timestamps were not passed.
-
-if ([System.String]::IsNullOrEmpty($startTime) -or [System.String]::IsNullOrEmpty($finishTime))
-{
-    $startTime   = "-na-"
-    $finishTime  = "-na-"
-    $elapsedTime = "-na-"
-}
-else
-{
-    $startTime   = [System.DateTime]::Parse($startTime).ToString("u")
-    $finishTime  = [System.DateTime]::Parse($finishTime).ToString("u")
-    $elapsedTime = $(New-TimeSpan $startTime $finishTime).ToString("c")
-}
-
-# Determine the workflow run URI.
-
-$workflowRunUri = "$env:GITHUB_SERVER_URL/$env:GITHUB_REPOSITORY/actions/runs/$env:GITHUB_RUN_ID"
-
-# Convert [$workflowRef] into the URI to referencing the correct workflow branch.  We're
-# going to use the GITHUB_REF environment variable.  This includes the branch like:
-#
-#       refs/heads/master
-#
-# Note that the workflow may be executing on a different branch than the repo build.
-
-if (!$workflowRef.Contains("/blob/master/"))
-{
-    throw "[workflow-ref=$workflowRef] is missing '/blob/master/'."
-}
-
-$githubRef      = $env:GITHUB_REF
-$lastSlashPos   = $githubRef.LastIndexOf("/")
-$workflowBranch = $githubRef.Substring($lastSlashPos + 1)
-$workflowUri    = $workflowRef.Replace("/blob/master/", "/blob/$workflowBranch/")
-
-# Determine the reason why the workflow was started based on the GITHUB_EVENT_NAME
-# and GITHUB_ACTOR environment variables.
-
-$event = $env:GITHUB_EVENT_NAME
-$actor = $env:GITHUB_ACTOR
-
-if (![System.String]::IsNullOrEmpty($actor))
-{
-    $actor = $actor.ToUpper()
-}
-
-if (![System.String]::IsNullOrEmpty($event))
-{
-    $event = $event.ToUpper()
-}
-
-if ($event -eq "workflow_dispatch")
-{
-    $reason = "Started by: **$actor**"
-}
-else
-{
-    $reason = "Event trigger: **$event**"
-}
-
-# Set the theme color based on the build outcome/success inputs.
-
-$themeColor = "ff0000" # green
-
-Switch ($buildOutcome)
-{
-    "success"
+    if (!$sendAlways -and !$sendOn.Contains($buildOutcome))
     {
-        $themeColor = "00ff00" # green
+        if ($buildSuccess -and sendOn.Contains("build-success"))
+        {
+            # Send the notification below.
+        }
+        elseif (!$buildSuccess -and sendOn.Contains("build-fail"))
+        {
+            # Send the notification below.
+        }
+        else 
+        {
+            # Don't send the notification below.
+
+            return
+        }
     }
 
-    "cancelled"
+    # Handle missing [build-branch] and [build-commit-uri] inputs.
+
+    if ([System.String]::IsNullOrEmpty($buildBranch))
     {
-        $themeColor = "ffa500" # orange
+        $buildBranch = "-na-"
+    }
+    else
+    {
+        $buildBranch = "**$buildBranch**"
     }
 
-    "skipped"
+    if ([System.String]::IsNullOrEmpty($buildCommit) -or [System.String]::IsNullOrEmpty($buildCommitUri))
     {
-        $themeColor = "ffa500" # orange
+        $buildCommitUri = "-na-"
+    }
+    else
+    {
+        $buildCommitUri = "[$buildCommit]($buildCommitUri)"
     }
 
-    "failure"
+    # Parse the optional start/finish times and compute the elapsed time.  Note that
+    # we're going to display "-na" when either of these timestamps were not passed.
+
+    if ([System.String]::IsNullOrEmpty($startTime) -or [System.String]::IsNullOrEmpty($finishTime))
     {
-        $themeColor = "ff0000" # red
+        $startTime   = "-na-"
+        $finishTime  = "-na-"
+        $elapsedTime = "-na-"
     }
-}
+    else
+    {
+        $startTime   = [System.DateTime]::Parse($startTime).ToString("u")
+        $finishTime  = [System.DateTime]::Parse($finishTime).ToString("u")
+        $elapsedTime = $(New-TimeSpan $startTime $finishTime).ToString("c")
+    }
 
-# Format $buildOutcome
+    # Determine the workflow run URI.
 
-$buildOutcome = "**$buildOutcome**"
+    $workflowRunUri = "$env:GITHUB_SERVER_URL/$env:GITHUB_REPOSITORY/actions/runs/$env:GITHUB_RUN_ID"
 
-# This is the legacy MessageCard format (Adaptive Cards are not supported by
-# the Team Connector at this time):
-#
-#   https://docs.microsoft.com/en-us/outlook/actionable-messages/message-card-reference
+    # Convert [$workflowRef] into the URI to referencing the correct workflow branch.  We're
+    # going to use the GITHUB_REF environment variable.  This includes the branch like:
+    #
+    #       refs/heads/master
+    #
+    # Note that the workflow may be executing on a different branch than the repo build.
 
-$card = 
+    if (!$workflowRef.Contains("/blob/master/"))
+    {
+        throw "[workflow-ref=$workflowRef] is missing '/blob/master/'."
+    }
+
+    $githubRef      = $env:GITHUB_REF
+    $lastSlashPos   = $githubRef.LastIndexOf("/")
+    $workflowBranch = $githubRef.Substring($lastSlashPos + 1)
+    $workflowUri    = $workflowRef.Replace("/blob/master/", "/blob/$workflowBranch/")
+
+    # Determine the reason why the workflow was started based on the GITHUB_EVENT_NAME
+    # and GITHUB_ACTOR environment variables.
+
+    $event = $env:GITHUB_EVENT_NAME
+    $actor = $env:GITHUB_ACTOR
+
+    if (![System.String]::IsNullOrEmpty($actor))
+    {
+        $actor = $actor.ToUpper()
+    }
+
+    if (![System.String]::IsNullOrEmpty($event))
+    {
+        $event = $event.ToUpper()
+    }
+
+    if ($event -eq "workflow_dispatch")
+    {
+        $reason = "Started by: **$actor**"
+    }
+    else
+    {
+        $reason = "Event trigger: **$event**"
+    }
+
+    # Set the theme color based on the build outcome/success inputs.
+
+    $themeColor = "ff0000" # green
+
+    Switch ($buildOutcome)
+    {
+        "success"
+        {
+            $themeColor = "00ff00" # green
+        }
+
+        "cancelled"
+        {
+            $themeColor = "ffa500" # orange
+        }
+
+        "skipped"
+        {
+            $themeColor = "ffa500" # orange
+        }
+
+        "failure"
+        {
+            $themeColor = "ff0000" # red
+        }
+    }
+
+    # Format $buildOutcome
+
+    $buildOutcome = "**$buildOutcome**"
+
+    # This is the legacy MessageCard format (Adaptive Cards are not supported by
+    # the Team Connector at this time):
+    #
+    #   https://docs.microsoft.com/en-us/outlook/actionable-messages/message-card-reference
+
+    $card = 
 @'
 {
     "@type": "MessageCard",
@@ -226,42 +244,48 @@ $card =
         }
     ],
     "potentialAction": [
-      {
-          "@type": "OpenUri",
-          "name": "Show Workflow Run",
-          "targets": [
+        {
+            "@type": "OpenUri",
+            "name": "Show Workflow Run",
+            "targets": [
             {
                 "os": "default",
                 "uri": "@workflow-run-uri"
             }
-          ]
-      },
-      {
-          "@type": "OpenUri",
-          "name": "Show Workflow",
-          "targets": [
+            ]
+        },
+        {
+            "@type": "OpenUri",
+            "name": "Show Workflow",
+            "targets": [
             {
                 "os": "default",
                 "uri": "@workflow-uri"
             }
-          ]
-      }
+            ]
+        }
     ]
 }    
 '@
 
-$card = $card.Replace("@operation", $operation)
-$card = $card.Replace("@reason", $reason)
-$card = $card.Replace("@runner", $env:COMPUTERNAME)
-$card = $card.Replace("@build-branch", $buildBranch)
-$card = $card.Replace("@build-commit-uri", $buildCommitUri)
-$card = $card.Replace("@build-outcome", $buildOutcome.ToUpper())
-$card = $card.Replace("@workflow-run-uri", $workflowRunUri)
-$card = $card.Replace("@workflow-uri", $workflowUri)
-$card = $card.Replace("@finish-time", $finishTime)
-$card = $card.Replace("@elapsed-time", $elapsedTime)
-$card = $card.Replace("@theme-color", $themeColor)
+    $card = $card.Replace("@operation", $operation)
+    $card = $card.Replace("@reason", $reason)
+    $card = $card.Replace("@runner", $env:COMPUTERNAME)
+    $card = $card.Replace("@build-branch", $buildBranch)
+    $card = $card.Replace("@build-commit-uri", $buildCommitUri)
+    $card = $card.Replace("@build-outcome", $buildOutcome.ToUpper())
+    $card = $card.Replace("@workflow-run-uri", $workflowRunUri)
+    $card = $card.Replace("@workflow-uri", $workflowUri)
+    $card = $card.Replace("@finish-time", $finishTime)
+    $card = $card.Replace("@elapsed-time", $elapsedTime)
+    $card = $card.Replace("@theme-color", $themeColor)
 
-# Post the card to Microsoft Teams.
+    # Post the card to Microsoft Teams.
 
-Invoke-WebRequest -Method "POST" -Uri $channel -ContentType "application/json" -Body $card | Out-Null
+    Invoke-WebRequest -Method "POST" -Uri $channel -ContentType "application/json" -Body $card | Out-Null
+}
+catch
+{
+    Write-ActionException $_
+    exit 1
+}
